8e8a7b36c9d875505e771a5a7eff6069603753fb
JunxuLiu <liujunxu@ly-sky.com>
2019-04-03 16:28:54 +0800

    添加注释信息 1、eureka注册中心注解配置 2、hystrix监视窗注解配置以及配置信息

--- hystrix/src/main/java/com/cloud/hytrix/HytrixApplication.java
+++ hystrix/src/main/java/com/cloud/hytrix/HytrixApplication.java
@@ -6,8 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-
+/*声明Hystrix监视窗应用，实现服务状态监视*/
 @EnableHystrixDashboard
+
 @SpringBootApplication
 public class HytrixApplication {
 
