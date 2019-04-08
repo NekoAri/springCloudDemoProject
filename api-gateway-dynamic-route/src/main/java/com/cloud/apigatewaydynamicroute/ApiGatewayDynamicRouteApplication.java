a3eea439ddc3d68298bbc1f46e9f551e8de753b6
JunxuLiu <liujunxu@ly-sky.com>
2019-04-08 14:04:24 +0800

    新增config-server

---
+++ api-gateway-dynamic-route/src/main/java/com/cloud/apigatewaydynamicroute/ApiGatewayDynamicRouteApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.apigatewaydynamicroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayDynamicRouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayDynamicRouteApplication.class, args);
+    }
+
+}
