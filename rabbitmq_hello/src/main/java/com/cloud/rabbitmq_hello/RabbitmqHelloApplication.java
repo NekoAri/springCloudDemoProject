d65aacbfae06775c7db8225442b9bf2ca76387c7
JunxuLiu <liujunxu@ly-sky.com>
2019-04-12 15:40:35 +0800

    添加消息总线

---
+++ rabbitmq_hello/src/main/java/com/cloud/rabbitmq_hello/RabbitmqHelloApplication.java
@@ -0,0 +1,15 @@
+package com.cloud.rabbitmq_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RabbitmqHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqHelloApplication.class, args);
+    }
+
+}
