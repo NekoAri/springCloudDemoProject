21562786319edf9991633f79af30cbf00b63c26e
JunxuLiu <liujunxu@ly-sky.com>
2019-05-06 09:11:33 +0800

    添加追踪

---
+++ trace_test/src/test/java/com/cloud/trace_test/TraceTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloud.trace_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TraceTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
